d5713042bfef058559f716259a3f42c915c1595e
Abdur Rehman <abdurrehmansaleem5@gmail.com>
2023-08-12 12:46:31 +0500

    Update

--- backend/index.js
+++ backend/index.js
@@ -37,9 +37,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running at ${process.env.PORT}`);
-    });
+    // app.listen(process.env.PORT, () => {
+    //   console.log(`Server is running at ${process.env.PORT}`);
+    // });
   })
   .catch((err) => {
     console.log(err);
